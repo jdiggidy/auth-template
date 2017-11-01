24153847d19308403049041d46c2bd8958aa42ac
jeremybarger <jeremyjusticebarger@gmail.com>
2017-11-01 12:41:34 -0600

    cleanup

--- Package.swift
+++ Package.swift
@@ -26,27 +26,3 @@ let package = Package(
     ]
 )
 
-
-/*
-import PackageDescription
-
-let package = Package(
-    name: "VaporApp",
-    targets: [
-        Target(name: "App"),
-        Target(name: "Run", dependencies: ["App"]),
-    ],
-    dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/auth-provider.git", majorVersion: 1)
-    ],
-    exclude: [
-        "Config",
-        "Database",
-        "Localization",
-        "Public",
-        "Resources",
-    ]
-)
-*/
