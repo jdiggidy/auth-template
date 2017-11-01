d7101f03fd84e6a208612913e45519e2e38b8ec7
jeremybarger <jeremyjusticebarger@gmail.com>
2017-11-01 12:32:00 -0600

    update spm to Swift4

--- Package.swift
+++ Package.swift
@@ -1,3 +1,33 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "VaporApp",
+    products: [
+        .library(name: "AuthTemplate", targets: ["AuthTemplate"]),
+        .executable(name: "Run", targets: ["Run"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.2.0")),
+        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.1.0")),
+        .package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.2.1")),
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["Vapor", "LeafProvider", "AuthProvider"],
+                exclude: [
+                    "Config",
+                    "Database",
+                    "Public",
+                    "Resources"
+            ]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+    ]
+)
+
+
+/*
 import PackageDescription
 
 let package = Package(
@@ -19,3 +49,4 @@ let package = Package(
         "Resources",
     ]
 )
+*/
